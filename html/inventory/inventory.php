a9aa4d7eb2f225418f0058ba3029f3da6adc48cb
kelscey90 <50043235+kelscey90@users.noreply.github.com>
2019-06-24 16:12:07 +0800

    Add files via upload

---
+++ html/inventory/inventory.php
@@ -0,0 +1,107 @@
+<?php
+	include '../header.php';
+	include '../main_content_header.php';
+?>
+	<div class="reports flex wrap">
+		<div class="cards one">
+			<a href="#">
+				<div class="cards_info">
+					<div class="left_card">
+						<div class="card_pack">
+							<label class="bold">2</label>
+							<p>Deliveries</p>
+						</div>
+					</div>
+
+					<div class="right_card">
+						<div class="card_pack">
+							<i class="fas fa-truck-moving fa-4x"></i>
+						</div>
+					</div>
+				</div>
+
+				<div class="button flex">
+					<div class="card_pack">
+						<span>More Information</span>
+					</div>
+				</div>	
+			</a>
+		</div>
+
+		<div class="cards two">
+			<a href="#">
+				<div class="cards_info">
+					<div class="left_card">
+						<div class="card_pack">
+							<label class="bold">5</label>
+							<p>Pending Requests</p>
+						</div>
+					</div>
+
+					<div class="right_card">
+						<div class="card_pack">
+							<i class="fas fa-people-carry fa-4x"></i>
+						</div>
+					</div>
+				</div>
+				<div class="button flex">
+					<div class="card_pack">
+						<span>More Information</span>
+					</div>
+				</div>	
+			</a>
+		</div>
+
+		<div class="cards three">
+			<a href="#">
+				<div class="cards_info">
+					<div class="left_card">
+						<div class="card_pack">
+							<label class="bold">0</label>
+							<p>Out of Stock</p>
+						</div>
+					</div>
+
+					<div class="right_card">
+						<div class="card_pack">
+							<i class="fas fa-exclamation-triangle fa-4x"></i>
+						</div>
+					</div>
+				</div>
+
+				<div class="button flex">
+					<div class="card_pack">
+						<span>More Information</span>
+					</div>
+				</div>	
+			</a>
+		</div>
+
+		<div class="cards four">
+			<a href="#">
+				<div class="cards_info">
+					<div class="left_card">
+						<div class="card_pack">
+							<label class="bold">0</label>
+							<p>Added Stock</p>
+						</div>
+					</div>
+
+					<div class="right_card">
+						<div class="card_pack">
+							<i class="fas fa-truck-loading fa-4x"></i>
+						</div>
+					</div>
+				</div>
+
+				<div class="button flex">
+					<div class="card_pack">
+						<span>More Information</span>
+					</div>
+				</div>	
+			</a>
+		</div>
+	</div>
+<?php
+	include '../footer.php';
+?>
---
+++ html/inventory/manage.php
@@ -0,0 +1,225 @@
+<?php
+	include '../header.php';
+	include '../main_content_header.php';
+?>
+
+	<div class="page">
+		<div class="manage_head">
+			<a href="add_item.php" class="create_btn"><i class="fas fa-plus"></i> Add Item</a>
+		</div>
+
+		<div class="manage_table">
+			<table>
+				<thead>
+					<tr>
+						<th>Item Code</th>
+						<th>Item Name</th>
+						<th>Category</th>
+						<th>Qty</th>
+						<th>UOM</th>
+						<th>Buying Price</th>
+						<th>Selling Price</th>
+						<th>Action</th>
+					</tr>
+				</thead>
+
+				<tbody>
+					<tr>
+						<td>IC-001</td>
+						<td>Samsung 32" Monitor</td>
+						<td>Output</td>
+						<td>25</td>
+						<td>Pc/s</td>
+						<td>5,000</td>
+						<td>10,000</td>
+						<td>
+							<div class="action flex">
+								<div class="edit_btn">
+									<a href="#">
+										<i class="far fa-edit"></i>
+									</a>
+								</div>
+
+								<div class="del_btn">
+									<a href="##">
+										<i class="fas fa-trash-alt"></i>
+									</a>
+								</div>
+
+								<div class="add_btn">
+									<a href="###">
+										<i class="fas fa-plus"><span>Stock Update</span></i>
+									</a>
+								</div>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>IC-001</td>
+						<td>Samsung 32" Monitor</td>
+						<td>Output</td>
+						<td>25</td>
+						<td>Pc/s</td>
+						<td>5,000</td>
+						<td>10,000</td>
+						<td>
+							<div class="action flex">
+								<div class="edit_btn">
+									<a href="#">
+										<i class="far fa-edit"></i>
+									</a>
+								</div>
+
+								<div class="del_btn">
+									<a href="##">
+										<i class="fas fa-trash-alt"></i>
+									</a>
+								</div>
+
+								<div class="add_btn">
+									<a href="###">
+										<i class="fas fa-plus"><span>Stock Update</span></i>
+									</a>
+								</div>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>IC-001</td>
+						<td>Samsung 32" Monitor</td>
+						<td>Output</td>
+						<td>25</td>
+						<td>Pc/s</td>
+						<td>5,000</td>
+						<td>10,000</td>
+						<td>
+							<div class="action flex">
+								<div class="edit_btn">
+									<a href="#">
+										<i class="far fa-edit"></i>
+									</a>
+								</div>
+
+								<div class="del_btn">
+									<a href="##">
+										<i class="fas fa-trash-alt"></i>
+									</a>
+								</div>
+
+								<div class="add_btn">
+									<a href="###">
+										<i class="fas fa-plus"><span>Stock Update</span></i>
+									</a>
+								</div>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>IC-001</td>
+						<td>Samsung 32" Monitor</td>
+						<td>Output</td>
+						<td>25</td>
+						<td>Pc/s</td>
+						<td>5,000</td>
+						<td>10,000</td>
+						<td>
+							<div class="action flex">
+								<div class="edit_btn">
+									<a href="#">
+										<i class="far fa-edit"></i>
+									</a>
+								</div>
+
+								<div class="del_btn">
+									<a href="##">
+										<i class="fas fa-trash-alt"></i>
+									</a>
+								</div>
+
+								<div class="add_btn">
+									<a href="###">
+										<i class="fas fa-plus"><span>Stock Update</span></i>
+									</a>
+								</div>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>IC-001</td>
+						<td>Samsung 32" Monitor</td>
+						<td>Output</td>
+						<td>25</td>
+						<td>Pc/s</td>
+						<td>5,000</td>
+						<td>10,000</td>
+						<td>
+							<div class="action flex">
+								<div class="edit_btn">
+									<a href="#">
+										<i class="far fa-edit"></i>
+									</a>
+								</div>
+
+								<div class="del_btn">
+									<a href="##">
+										<i class="fas fa-trash-alt"></i>
+									</a>
+								</div>
+
+								<div class="add_btn">
+									<a href="###">
+										<i class="fas fa-plus"><span>Stock Update</span></i>
+									</a>
+								</div>
+							</div>
+						</td>
+					</tr>
+					<tr>
+						<td>IC-001</td>
+						<td>Samsung 32" Monitor</td>
+						<td>Output</td>
+						<td>25</td>
+						<td>Pc/s</td>
+						<td>5,000</td>
+						<td>10,000</td>
+						<td>
+							<div class="action flex">
+								<div class="edit_btn">
+									<a href="#">
+										<i class="far fa-edit"></i>
+									</a>
+								</div>
+
+								<div class="del_btn">
+									<a href="##">
+										<i class="fas fa-trash-alt"></i>
+									</a>
+								</div>
+
+								<div class="add_btn">
+									<a href="###">
+										<i class="fas fa-plus"><span>Stock Update</span></i>
+									</a>
+								</div>
+							</div>
+						</td>
+					</tr>
+				</tbody>
+			</table>
+		</div>
+
+	<div style="height: 20px; padding: 20px 10px">
+		<div class="left">
+			<p>Showing 1 to 3 entries of 3 entries</p>
+		</div>
+
+		<div class="right">
+			<p>Page 1</p>
+		</div>
+	</div>
+
+	</div>
+
+<?php
+	include '../footer.php';
+?>
